32343d77e3d120fad68e0b63098000a863d40819
Austin Lancaster <aus.lancaster@gmail.com>
2017-11-25 11:09:25 +1100

    Full animationg for randomize effect and world initialization

--- Assets/Standard Assets/Environment/Scripts/RandomizeController.cs
+++ Assets/Standard Assets/Environment/Scripts/RandomizeController.cs
@@ -18,17 +18,33 @@ public class RandomizeController : MonoBehaviour {
     private AnimationCurve fov;
     [SerializeField]
     private AnimationCurve timeScale;
+    [SerializeField]
+    private Vingette vingette;
+    [SerializeField]
+    private AnimationCurve minRadius;
+    [SerializeField]
+    private AnimationCurve maxRadius;
+    [SerializeField]
+    private AnimationCurve saturation;
+    [SerializeField]
+    private AnimationCurve minRadiusBegin;
+    [SerializeField]
+    private AnimationCurve maxRadiusBegin;
+    [SerializeField]
+    private AnimationCurve saturationBegin;
 
 
     private bool randomizing = false;
     private bool triggered = false;
     private bool triggering = false;
     private float randomizeTime = 0.8f;
-    private float totalTime = 1.2f;
+    private float totalRandomizeTime = 1.2f;
+    private float totalBeginTime = 6.0f;
 
 // Use this for initialization
 void Start () {
         generator.CreateTerrainChunk();
+        StartCoroutine(Begin());
     }
 
     // Update is called once per frame
@@ -53,10 +69,13 @@ void Start () {
         triggering = false;
         triggered = false;
 
-        for (float t = 0; t < totalTime || triggered != true; t += Time.unscaledDeltaTime * 1.2f)
+        for (float t = 0; t < totalRandomizeTime || triggered != true; t += Time.unscaledDeltaTime * 1.2f)
         {
-            print(t);
             camera.fieldOfView = fov.Evaluate(t);
+            Time.timeScale = timeScale.Evaluate(t);
+            vingette.minRadius = minRadius.Evaluate(t);
+            vingette.maxRadius = maxRadius.Evaluate(t);
+            vingette.saturation = saturation.Evaluate(t);
 
             if (triggering)
             {
@@ -73,7 +92,28 @@ void Start () {
 
             yield return null;
         }
-        camera.fieldOfView = fov.Evaluate(totalTime);
+        camera.fieldOfView = fov.Evaluate(totalRandomizeTime);
+        Time.timeScale = timeScale.Evaluate(totalRandomizeTime);
+        vingette.minRadius = minRadius.Evaluate(totalRandomizeTime);
+        vingette.maxRadius = maxRadius.Evaluate(totalRandomizeTime);
+        vingette.saturation = saturation.Evaluate(totalRandomizeTime);
+
         randomizing = false;
+    }
+
+    IEnumerator Begin()
+    {
+
+        for (float t = 0; t < totalBeginTime && !randomizing; t += Time.unscaledDeltaTime * 1.2f)
+        {
+            vingette.minRadius = minRadiusBegin.Evaluate(t);
+            vingette.maxRadius = maxRadiusBegin.Evaluate(t);
+            vingette.saturation = saturationBegin.Evaluate(t);
+
+            yield return null;
+        }
+        vingette.minRadius = minRadiusBegin.Evaluate(totalBeginTime);
+        vingette.maxRadius = maxRadiusBegin.Evaluate(totalBeginTime);
+        vingette.saturation = saturationBegin.Evaluate(totalBeginTime);
     }
 }
---
+++ Assets/Standard Assets/Environment/Scripts/Vingette.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Vingette : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _shader;
+    [Range(0, 1)]
+    public float minRadius;
+    [Range(0, 1)]
+    public float maxRadius;
+    [Range(0, 1)]
+    public float saturation;
+
+    Material _material;
+
+    void OnEnable()
+    {
+        _material = new Material(_shader);
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        _material.SetFloat("_MinRadius", minRadius);
+        _material.SetFloat("_MaxRadius", maxRadius);
+        _material.SetFloat("_Saturation", saturation);
+
+        Graphics.Blit(src, dst, _material, 0);
+    }
+}
